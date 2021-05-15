af8b16420edab1d49e242296cf991b8a0e948a7b
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-05-15 23:05:41 +0900

    Add startup audio and speaker profile image

--- src/HelloWorld/SessionInfo.tsx
+++ src/HelloWorld/SessionInfo.tsx
@@ -1,5 +1,11 @@
 import { interpolate, spring, useCurrentFrame, useVideoConfig, Img } from 'remotion';
 import logo from '../Images/ubuCon_logo_or.svg';
+import profile from '../Images/profile_placeholder.png';
+import startup from '../Sounds/startup.mp3';
+import {Audio} from 'remotion';
+
+
+
 
 export const SessionInfo: React.FC<{
 	transitionStart: number;
@@ -17,9 +23,24 @@ export const SessionInfo: React.FC<{
 				marginTop: marginTop,
 				opacity: opacity,
 				fontFamily: 'Ubuntu',
+				color: 'white',
 				paddingLeft: 30
 			}}>
-			<Img src={logo} style={{ width: 500 }} />
+				<Audio src={startup} />
+			<div style={{
+				display: 'flex',
+				flexDirection: 'row',
+				height: 200
+			}}>
+				<Img src={logo} style={{ width: 500 }}/>
+				<span style={{flex: 1}}></span>
+				<Img src={profile} style={{ 
+					width: 200, height: 200,
+					borderRadius: 200 / 2,
+    			overflow: "hidden",
+					marginRight: 40
+					}} />
+			</div>
 			<div style={{
 				marginLeft: 30,
 				display: 'flex',
@@ -34,9 +55,16 @@ export const SessionInfo: React.FC<{
 				<span style={{
 					fontSize: 40,
 					marginTop: 20,
-					marginLeft: 10
+					marginLeft: 10,
+					fontWeight: 'bold'
 				}}>
 					Speaker name here
+					</span>
+					<span style={{
+					fontSize: 40,
+					marginLeft: 10,
+				}}>
+					Speaker bio here
 					</span>
 			</div>
 
