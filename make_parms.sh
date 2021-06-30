98706f1a6f281a9f4a465a9c40781469a3e20902
Gyuseok Jung <gsjung@whatap.io>
2021-06-30 13:31:00 +0900

    add sample url

--- make_parms.sh
+++ make_parms.sh
@@ -6,6 +6,6 @@ do
     echo $video
     gsed '2s/.*/'${video}'/g' param_base.json > param$i.json
     i=$((i + 1))
-    npx remotion render src/index.tsx VideoSeqs video$i.mp4 --props=param$i.json
+    npx remotion render src/index.tsx VideoSeqs vdieos/video$i.mp4 --props=param$i.json
 done < video_list.txt
 
