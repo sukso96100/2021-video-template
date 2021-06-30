f854e1db3ea705db05924f3bc81052456d72004f
Gyuseok Jung <epdlemflaj@gmail.com>
2021-06-30 13:38:17 +0900

    Update make_parms.sh

--- make_parms.sh
+++ make_parms.sh
@@ -4,7 +4,7 @@ i=1
 while read video
 do
     echo $video
-    gsed '2s/.*/'${video}'/g' param_base.json > param$i.json
+    sed '2s/.*/'${video}'/g' param_base.json > param$i.json
     i=$((i + 1))
     npx remotion render src/index.tsx VideoSeqs vdieos/video$i.mp4 --props=param$i.json
 done < video_list.txt
