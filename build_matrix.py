dc71f54873361dfb81506e7d9076576c357f4021
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-07-21 21:18:21 +0900

    Add integration with GCP storage bucket

--- build_matrix.py
+++ build_matrix.py
@@ -2,12 +2,16 @@ import os
 import json
 
 include = []
-files = os.listdir("params")
+lsdir = "params/"
+files = os.listdir(lsdir)
 for i in files:
   if i.endswith(".json"):
-    include.append({
-        "path": i,
-        "output": i.replace(".json", ".mp4")
-    })
+    with open(lsdir+i) as json_file:
+      json_data = json.load(json_file)
+      include.append({
+          "path": i,
+          "output": i.replace(".json", ".mp4"),
+          "videoPath": json_data["videoPath"]
+      })
 matrix = { "include": include }
 json.dump(matrix, open("matrix.json", "w"), indent=4)
